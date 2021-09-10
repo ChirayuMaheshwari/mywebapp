51c1def1f183ababf8f75a8ce550336155bb5853
ChirayuMaheshwari <chirayumaheshwari5@gmai.com>
2021-09-10 14:27:28 +0530

    adding result page

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express=require('express');
 const app=express();
 app.set('view engine','ejs');
 app.listen(process.env.PORT||8080)
-
+var bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended: false}));
 const { Client } = require('pg');
 
 const client = new Client({
@@ -14,15 +15,13 @@ const client = new Client({
 
 client.connect();
 
-console.log(client);
+
 
 app.get('/',function(req,res){
-    client.query("insert into my_info(name) values($1);",['chirayu'],function(err,result){
-        if(err)
-        console.log(err);
-        else
-        console.log(result);
-    })
     res.render(__dirname+'//views//index.ejs');
     //res.send({name:'chirayu'});
+})
+
+app.post('/data',function(req,res){
+res.render(__dirname+'//views//result.ejs',{name:req.body.name});
 })
