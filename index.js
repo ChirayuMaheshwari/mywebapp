e5259f53c5b3284460a8d84ed451a86b700efc69
ChirayuMaheshwari <chirayumaheshwari5@gmai.com>
2021-09-10 13:20:26 +0530

    push the code

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ const app=express();
 app.set('view engine','ejs');
 app.listen(process.env.PORT||8080)
 app.get('/',function(req,res){
-    res.send({name:'chirayu'});
+    res.render(__dirname+'\views\index.ejs');
+    //res.send({name:'chirayu'});
 })
