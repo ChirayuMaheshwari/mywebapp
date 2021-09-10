4aa68ce1b241c97472b131f0157454cd81990359
ChirayuMaheshwari <chirayumaheshwari5@gmai.com>
2021-09-10 23:38:22 +0530

    adding login/signup

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ const { Client } = require('pg');
 
 const client = new Client({
   connectionString: process.env.DATABASE_URL|| 'postgres://postgres:@localhost:5432/postgres',
-  ssl: {
-    rejectUnauthorized: false
-  }
+  // ssl: {
+  //   rejectUnauthorized: false
+  // }
 });
 
 client.connect();
